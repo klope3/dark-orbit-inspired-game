8ed4877a3ed9311dcac3a47a7af5367e636649a0
Josh Klope <klope3@msn.com>
2023-10-07 10:41:51 -0500

    Allow spawned enemies to use weapons

--- Assets/Scripts/Weapon/Weapon.cs
+++ Assets/Scripts/Weapon/Weapon.cs
@@ -7,7 +7,30 @@ public class Weapon : MonoBehaviour
 {
     [SerializeField] private Transform muzzleLocation;
     [SerializeField] private GameObjectPool projectilePool;
+    [SerializeField, Tooltip("Whether to try and find a pool on Awake. " +
+        "Useful for weapons instantiated at runtime, since these won't have " +
+        "a reference to a pool yet.")] private bool findPool;
+    [SerializeField, Tooltip("The tag to use for finding a pool on Awake. " +
+        "Does nothing if findPool is false.")] private string findPoolTag;
     [SerializeField] private bool showAimLine;
+
+    private void Awake()
+    {
+        if (findPool)
+        {
+            string findError = $"The weapon attached to {gameObject.name} couldn't find a GameObjectPool with the tag {findPoolTag}!";
+            GameObject objWithTag = GameObject.FindGameObjectWithTag(findPoolTag);
+            if (objWithTag == null)
+            {
+                Debug.LogWarning(findError);
+            }
+            projectilePool = objWithTag.GetComponent<GameObjectPool>();
+            if (projectilePool == null)
+            {
+                Debug.LogWarning(findError);
+            }
+        }
+    }
 
     private void OnDrawGizmos()
     {
